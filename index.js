6b9fd865a30d11dc537016777eb19fb2be51c75c
rohit20001221 <rohit20001221@gmail.com>
2023-10-21 19:38:58 +0530

    react webpack remote module helper

---
+++ index.js
@@ -0,0 +1,27 @@
+export const loadScope = (url, scope) => {
+  const element = document.createElement("script");
+  const promise = new Promise((resolve, reject) => {
+    element.src = url;
+    element.type = "text/javascript";
+    element.async = true;
+    element.onload = () => resolve(window[scope]);
+    element.onerror = reject;
+  });
+
+  document.head.appendChild(element);
+  return promise;
+};
+
+export const loadRemoteModule = async ({ url, scope, module }) => {
+  try {
+    const container = await loadScope(url, scope);
+
+    await __webpack_init_sharing__("default");
+    await container.init(__webpack_share_scopes__.default);
+
+    const factory = await container.get(module);
+    return factory();
+  } catch (error) {
+    throw error;
+  }
+};
